08db3b27a25c73db32a3002f5957c8763a979f2e
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-03-07 17:51:32 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Department dept = new Department(1, "Books");
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, dept);
+		
+		System.out.println(seller);
 	}
 
 }
