6797eccb714fada3aaa05fe5c4f292fdb6e5ec11
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-03-08 19:51:11 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,23 +22,37 @@ public class Program {
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
+		// ------------------------
 		
 		System.out.println("\n=== TEST 2: seller findByDepartment ===");
+		
 		Department department = new Department(2, null);	
 		List<Seller> list = sellerDao.findByDepartment(department);
 		
 		list.forEach(System.out::println);
+		// ------------------------
 		
 		System.out.println("\n=== TEST 3: seller findAll ===");
 		list = sellerDao.findAll();
 		
 		list.forEach(System.out::println);
+		// ------------------------
 		
 		System.out.println("\n=== TEST 4: seller insert ===");
+		
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.now(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		// ------------------------
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		
+		System.out.println("Update completed!");
 	}
 
 }
