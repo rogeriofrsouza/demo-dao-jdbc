f14875dbe3dd30ad314f517a1b812a8300ca354c
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2023-03-08 20:03:08 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,8 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
 		/*
 		 * Program não conhece a implementação, somente a interface
 		 * Injeção de dependência sem implementação explícita (Factory)
@@ -53,6 +56,16 @@ public class Program {
 		sellerDao.update(seller);
 		
 		System.out.println("Update completed!");
+		// ------------------------
+		
+		System.out.println("\n=== TEST 6: seller delete ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed!");
+		
+		sc.close();
 	}
 
 }
